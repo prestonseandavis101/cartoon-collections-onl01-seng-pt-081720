9327bdb2497d425aa9ea29dd90737d376f28c6db
Preston Davis <prestonseandavis101@yahoo.com>
2020-08-28 02:55:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |i, number|
+    puts "#{number+1} #{i}"
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veggies)
+  new_veggies = []
+  i = 0 
+  while i < veggies.length
+  new_veggies << veggies [i].capitalize + "!"
+  i += 1 
+end
+new_veggies
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long)
+  i = 0 
+  if calls_long.any? {|x|x.length>4}
+    return true 
+  else
+    return false
+    i = i + 1
+  end
 end
 
 def find_the_cheese# code an argument here
